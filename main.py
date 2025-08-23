6830136c26a562f9ab7c7fe9c722b24885fcc765
yugamax <ayp14111@gmail.com>
2025-08-24 03:25:37 +0530

    ok

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Groq LLM RAG Chatbot with Memory")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://status-ten-gules.vercel.app","*"],
+    allow_origins=["https://status-ten-gules.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
